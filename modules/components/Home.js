5800c2459a568effeb8be9ca0f6b18f75120b58c
Hannu Kärkkäinen <hannu.karkkainen@gmail.com>
2016-05-08 17:01:42 +0300

    Added board example impl

--- modules/components/Home.js
+++ modules/components/Home.js
@@ -3,6 +3,15 @@ import { Link } from 'react-router'
 import Title from 'react-title-component'
 
 class Home extends React.Component {
+
+  renderRow(row) {
+    return row.map((col, colIndex) => <div>{col}</div>)
+  }
+
+  renderBoard() {
+    return this.props.board.map((row, rowIndex) => this.renderRow(row, rowIndex))
+  }
+
   render() {
     return (
       <div>
@@ -13,9 +22,24 @@ class Home extends React.Component {
           <li><Link to="/dragon?level=3">Hard</Link></li>
           <li><Link to="/dragon?level=4">Very hard</Link></li>
         </ul>
+        { this.renderBoard() }
       </div>
     )
   }
+}
+
+Home.defaultProps = {
+  board: [
+    [0, 0, 1, 0, 0, 4, 0, 2, 0],
+    [0, 0, 1, 0, 0, 4, 0, 2, 0],
+    [0, 0, 1, 0, 0, 4, 0, 2, 0],
+    [0, 0, 1, 0, 0, 4, 0, 2, 0],
+    [0, 0, 1, 0, 0, 4, 0, 2, 0],
+    [0, 0, 1, 0, 0, 4, 0, 2, 0],
+    [0, 0, 1, 0, 0, 4, 0, 2, 0],
+    [0, 0, 1, 0, 0, 4, 0, 2, 0],
+    [0, 0, 1, 0, 0, 4, 0, 2, 0]
+  ]
 }
 
 export default Home
